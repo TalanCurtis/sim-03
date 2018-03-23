c077f78726144af86144e49448537c9b003f8fd8
Alan Curtis <talancurtis@gmail.com>
2018-03-23 14:46:10 -0600

    added profile and search view addeed to routs as well.

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 // import components
 import Auth from './views/Auth/Auth';
 import Home from './views/Home/Home';
+import Profile from './views/Profile/Profile';
+import Search from './views/Search/Search';
 
 export default (
     <Switch>
         <Route exact path='/' component={Auth} />
         <Route path='/Home' component={Home} />
+        <Route path='/Profile' component={Profile} />
+        <Route path='/Search' component={Search} />
     </Switch>
 )
